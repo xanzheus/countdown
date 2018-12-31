94bef51159fe1fb812b519bc033d50d6a27e3011
PNNutkung <pipatpol.t@ku.th>
2018-12-31 13:59:41 +0700

    Add font and condition to clock

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,3 +1,4 @@
+import Clock from './components/Clock'
 export { Base } from './components/Base'
 export { Card } from './components/Card'
 export { Container } from './components/Container'
@@ -5,3 +6,4 @@ export { Input } from './components/Input'
 export { Button } from './components/Button'
 export { Row } from './components/Row'
 export { Half } from './components/Half'
+export default Clock
